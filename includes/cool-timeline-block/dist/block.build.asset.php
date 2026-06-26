bc54faf3a60b1ac047e10a97e04711f5eeaec907
Siddharth7002 <siddharthcoolplugins@gmail.com>
2026-06-26 11:56:38 +0530

    commit build files

--- includes/cool-timeline-block/dist/block.build.asset.php
+++ includes/cool-timeline-block/dist/block.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3bae7463daa5cb6c9969');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e23d2eeaecc9537093e8');
