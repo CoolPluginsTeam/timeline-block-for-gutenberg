d7d6954624e6e57a6a5eb8939724d678825995d1
abhishekgorshi <abhishekgorshicoolplugins@gmail.com>
2026-07-15 15:54:11 +0530

    Class sanitization
    for layout class apply check

--- includes/cool-timeline-block/src/story-timeline/save.js
+++ includes/cool-timeline-block/src/story-timeline/save.js
@@ -20,7 +20,7 @@ export default function Save(props) {
 			{'' !== contentTimelineStyle( props ) &&
 			<style dangerouslySetInnerHTML={{ __html: contentTimelineStyle( props ) }}/>
 			}
-			<div className={`cool-${timelineLayout}-timeline-body ctlb-wrapper ${timelineDesign} ${Orientation}`}>
+			<div className={`cool-${['vertical', 'horizontal'].includes(timelineLayout) ? timelineLayout : 'vertical'}-timeline-body ctlb-wrapper ${['both-sided', 'one-sided'].includes(timelineDesign) ? timelineDesign : 'both-sided'} ${['left', 'right'].includes(Orientation) ? Orientation : 'left'}`}>
 		 		<div className="cool-timeline-block-list">
 				 <InnerBlocks.Content />
 		 		</div>
