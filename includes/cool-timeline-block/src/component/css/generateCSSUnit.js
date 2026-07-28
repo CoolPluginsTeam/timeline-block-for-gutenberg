90ea8a02245806d637151ffe44901445f5bdaa6a
abhishekgorshi <abhishekgorshicoolplugins@gmail.com>
2026-07-28 16:49:11 +0530

    use value sanitize
    use Unit value sanitize by import withUnit

--- includes/cool-timeline-block/src/component/css/generateCSSUnit.js
+++ includes/cool-timeline-block/src/component/css/generateCSSUnit.js
@@ -1,9 +1,11 @@
+import { withUnit } from "./sanitizeCSS.js";
 function generateCSSUnit ( value, unit ) {
 
 	var css = ""
 
 	if( typeof value != "undefined" ) {
-		css += value + unit
+		const safeValue = withUnit( value, unit, { min: 0, max: 1000, allowFloat: true } );
+		css += safeValue
 	}
 	
 	return css
